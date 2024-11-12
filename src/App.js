e2bafacf770261a5b6baa58be8d7f7e98bb19bea
Himanish <himanish.kaushal@gmail.com>
2024-11-12 13:33:50 -0500

    working react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Heading from "./Heading";
+
+const App = () =>{
+    return (
+       <>
+        <Heading />
+        <p>Welcome to React App thats build using Webpack and Babel separately.</p>
+      </>
+    )
+};
+
+export default App;
---
+++ src/Heading.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Heading = () =>{
+    return (
+        <h1>
+            This is the heading.
+        </h1>
+    )
+};
+
+export default Heading;
